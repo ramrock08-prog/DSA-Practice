1d9095b7667fc8d4c05d1d10d69653b1cb83db5d
Ram Charan <ramcharankorukonda08@gmail.com>
2026-08-18 21:16:08 +0530

    Time: 4 ms (51.82%) | Memory: 15 MB (25.04%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target){
         unordered_map<int,int> ram;
-        vector<int> jin;
         for(int i=0;i<nums.size();i++){
             int comp = target - nums[i];
             if(ram.find(comp) != ram.end()){
-               jin.push_back(ram[comp]);
-               jin.push_back(i);
+               return{ram[comp],i};
             }
             ram[nums[i]] = i;        
         }
-        return jin;
+        return {};
     }
 };
